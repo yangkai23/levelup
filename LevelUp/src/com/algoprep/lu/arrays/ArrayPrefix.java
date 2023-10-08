b0ef50c9c158954aec5a767cbf24a30df0c1309d
yangkai23 <tallurishanmukha.anirudh@clarivate.com>
2023-10-08 12:42:19 +0530

    arrayPrefix

--- LevelUp/src/com/algoprep/lu/arrays/ArrayPrefix.java
+++ LevelUp/src/com/algoprep/lu/arrays/ArrayPrefix.java
@@ -1,5 +1,24 @@
 package com.algoprep.lu.arrays;
 
-public class ArrayPrefix {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class ArrayPrefix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int ar[] = new int[scanner.nextInt()];
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = scanner.nextInt();
+        }
+        System.out.println(Arrays.toString(ar));
+        System.out.println(Arrays.toString(prefix(ar)));
+        scanner.close();
+    }
+
+    private static int[] prefix(int[] ar) {
+        for (int i = 1; i < ar.length; i++) {
+            ar[i] = ar[i] + ar[i - 1];
+        }
+        return ar;
+    }
 }

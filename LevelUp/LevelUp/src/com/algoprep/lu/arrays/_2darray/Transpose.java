d03e37427b773a4b8fdbdcfc9f5405465475afe8
yangkai23 <tallurishanmukha.anirudh@clarivate.com>
2023-10-15 22:37:34 +0530

    Transpose matrix

---
+++ LevelUp/LevelUp/src/com/algoprep/lu/arrays/_2darray/Transpose.java
@@ -0,0 +1,39 @@
+package com.algoprep.lu.arrays._2darray;
+
+import java.util.Scanner;
+
+public class Transpose {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] ar = new int[scanner.nextInt()][scanner.nextInt()];
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[i].length; j++) {
+                ar[i][j] = scanner.nextInt();
+            }
+        }
+        printMatrix(ar);
+        transpose(ar);
+        printMatrix(ar);
+        scanner.close();
+    }
+
+    private static void transpose(int[][] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[i].length; j++) {
+                if (i == j) break;
+                int temp = ar[i][j];
+                ar[i][j] = ar[j][i];
+                ar[j][i] = temp;
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[i].length; j++) {
+                System.out.print(ar[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

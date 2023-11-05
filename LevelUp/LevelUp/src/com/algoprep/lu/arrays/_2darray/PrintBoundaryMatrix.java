a8d297d0ac9be0d64e19af427c9e7758d753a068
yangkai23 <tallurishanmukha.anirudh@clarivate.com>
2023-11-05 23:32:26 +0530

    print spiral matrix

---
+++ LevelUp/LevelUp/src/com/algoprep/lu/arrays/_2darray/PrintBoundaryMatrix.java
@@ -0,0 +1,53 @@
+package com.algoprep.lu.arrays._2darray;
+
+import java.util.Scanner;
+
+public class PrintBoundaryMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] ar = new int[scanner.nextInt()][scanner.nextInt()];
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[i].length; j++) {
+                ar[i][j] = scanner.nextInt();
+            }
+        }
+        printMatrix(ar);
+        printBoundary(ar);
+        scanner.close();
+    }
+
+    public static void printBoundary(int[][] ar) {
+        int i = 0;
+        int j = 0;
+        int steps = ar.length - 1;
+        for (int k = 0; k < steps; k++) {
+            System.out.print(ar[i][j] + " ");
+            j++;
+        }
+
+        for (int k = 0; k < steps; k++) {
+            System.out.print(ar[i][j] + " ");
+            i++;
+        }
+
+        for (int k = 0; k < steps; k++) {
+            System.out.print(ar[i][j] + " ");
+            j--;
+        }
+
+        for (int k = 0; k < steps; k++) {
+            System.out.print(ar[i][j] + " ");
+            i--;
+        }
+
+    }
+
+    static void printMatrix(int[][] ar) {
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar[i].length; j++) {
+                System.out.print(ar[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}

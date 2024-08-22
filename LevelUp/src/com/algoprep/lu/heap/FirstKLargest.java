a79554de00aa7ef5d4df229cbacd6f1df1771f31
Shanmukha Anirudh <tallurishanmukha.anirudh@clarivate.com>
2024-08-22 08:13:48 +0530

    first k largest and smallest PQ

---
+++ LevelUp/src/com/algoprep/lu/heap/FirstKLargest.java
@@ -0,0 +1,29 @@
+package com.algoprep.lu.heap;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class FirstKLargest {
+    public static void main(String[] args) {
+        int[] ar = {5, 4, 8, 24, 1, 6};
+        int k = 4;
+        printKElements(ar, k);
+    }
+
+    private static void printKElements(int[] ar, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            pq.add(ar[i]);
+        }
+        int len = ar.length;
+        for (int i = k; i < len; i++) {
+            if (pq.peek() < ar[i]) {
+                pq.add(ar[i]);
+                pq.poll();
+            }
+        }
+        while (!pq.isEmpty()) {
+            System.out.println(pq.poll());
+        }
+    }
+}

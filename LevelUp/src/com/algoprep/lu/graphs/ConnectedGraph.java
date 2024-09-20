fd66963130a39c895b082968b9c25414a06ade67
Shanmukha Anirudh <tallurishanmukha.anirudh@clarivate.com>
2024-09-20 10:43:49 +0530

    is connected graph

--- LevelUp/src/com/algoprep/lu/graphs/ConnectedGraph.java
+++ LevelUp/src/com/algoprep/lu/graphs/ConnectedGraph.java
@@ -5,17 +5,30 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-public class BFS {
+public class ConnectedGraph {
     public static void main(String[] args) {
-        int[][] graph = {{0, 1, 1, 0, 0}, {1, 0, 1, 1, 0}, {1, 1, 0, 1, 1}, {0, 1, 1, 0, 1}, {0, 0, 1, 1, 0}};
+        int[][] graph = {
+                {0, 1, 1, 0, 0},
+                {1, 0, 1, 1, 0},
+                {1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
         List<List<Integer>> lists = listRepresentation(graph);
         System.out.println(lists);
-        bfs(lists, 0);
-
+        boolean isConnected=isConnected(lists, 0,2);
+        System.out.println(isConnected);
     }
 
-    private static void bfs(List<List<Integer>> graph, int i) {
+    private static boolean isConnected(List<List<Integer>> graph, int start, int dest) {
         boolean[] visited = new boolean[graph.size()];
+            bfs(graph, start,visited);
+           for(boolean isVisited:visited){
+               if(!isVisited)
+                   return false;
+           }
+            return true;
+    }
+    private static void bfs(List<List<Integer>> graph, int i, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(i);
         visited[i] = true;
@@ -30,7 +43,6 @@ public class BFS {
         }
 
     }
-
     public static List<List<Integer>> listRepresentation(int[][] graph) {
         List<List<Integer>> list = new ArrayList<>();
         for (int i = 0; i < graph.length; i++) {

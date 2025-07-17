6928f77ad5afaa8bd6c7c3b58f51d95854598675
Anirudh <amirudhshanmukha2399@gmail.com>
2025-07-18 00:11:27 +0530

    add CountNumberOfNiceSubarrays implementation and update workspace configuration

---
+++ untitled/src/slidingwindow/CountNumberOfNiceSubarrays.java
@@ -0,0 +1,41 @@
+package slidingwindow;
+
+/**
+ * @author Shanmukha Anirudh
+ * @date 17/07/25
+ */
+public class CountNumberOfNiceSubarrays {
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 1, 1};
+        System.out.println(numberOfSubarrays(nums, 3));
+    }
+
+    public static int numberOfSubarrays(int[] nums, int k) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] % 2;
+        }
+        return numberOfSubarrays1(nums, k) - numberOfSubarrays1(nums, k - 1);
+    }
+
+    public static int numberOfSubarrays1(int[] nums, int k) {
+        if (k < 0)
+            return 0;
+        int left = 0;
+        int right = 0;
+        int count = 0;
+        int sum = 0;
+        while (right < nums.length) {
+            sum += nums[right];
+            while (sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+            count += right - left + 1;
+
+            right++;
+        }
+        return count;
+    }
+}

8f1f321727d5af6b38744d8964b47864f52aacc4
Anirudh <amirudhshanmukha2399@gmail.com>
2025-10-03 15:59:37 +0530

    GasStation greedy

---
+++ untitled/src/greedy/GasStation.java
@@ -0,0 +1,36 @@
+package greedy;
+
+
+/**
+ * @author Anirudh
+ * @since 03/10/25
+ */
+public class GasStation {
+
+
+
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+        return total >= 0 ? start : -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] gas = {1,2,3,4,5};
+        int[] cost = {3,4,5,1,2};
+        GasStation st = new GasStation();
+        System.out.println(st.canCompleteCircuit(gas, cost));
+    }
+}

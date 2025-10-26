cf93dcbddaaef68f6fb184d3a95edba58a96d759
Anirudh <amirudhshanmukha2399@gmail.com>
2025-10-26 10:35:33 +0530

    binary search two sum II

---
+++ untitled/src/binarysearch/TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,22 @@
+package binarysearch;
+
+/**
+ * @author Anirudh
+ * @since 26/10/25
+ */
+public class TwoSumIIInputArrayIsSorted {
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target)
+                return new int[]{left + 1, right + 1};
+            else if (sum > target)
+                right--;
+            else
+                left++;
+        }
+        return new int[]{-1};
+    }
+}

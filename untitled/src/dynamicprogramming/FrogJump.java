8e0c40d5e53b1c618b42a53b110beb12471f3ad9
Anirudh <amirudhshanmukha2399@gmail.com>
2025-12-21 18:14:55 +0530

    frog jump using tabulation and removing space

--- untitled/src/dynamicprogramming/FrogJump.java
+++ untitled/src/dynamicprogramming/FrogJump.java
@@ -12,26 +12,62 @@ public class FrogJump {
         int[] dp = new int[n + 1];
 
         Arrays.fill(dp, -1);
-        return getCost(n - 1, height, dp);
+        return getCostUsingTabulation(n, height, dp);
 
     }
 
-    static int getCost(int n, int[] height, int[] dp) {
+    static int getCostUsingMemoization(int n, int[] height, int[] dp) {
         if (n == 0) {
             return 0;
         }
 
         if (dp[n] != -1) return dp[n];
 
-        int left = getCost(n - 1, height, dp) + Math.abs(height[n] - height[n - 1]);
+        int left = getCostUsingMemoization(n - 1, height, dp) + Math.abs(height[n] - height[n - 1]);
 
         int right = -1;
         if (n > 1) {
-            right = getCost(n - 2, height, dp) + Math.abs(height[n] - height[n - 2]);
+            right = getCostUsingMemoization(n - 2, height, dp) + Math.abs(height[n] - height[n - 2]);
         }
 
         if (right != -1) dp[n] = Math.min(left, right);
         else dp[n] = left;
         return dp[n];
+    }
+
+    static int getCostUsingTabulation(int n, int[] height, int[] dp) {
+        dp[0] = 0;
+        for (int i = 1; i < n; i++) {
+            int left = dp[i - 1] + Math.abs(height[i] - height[i - 1]);
+            int right = Integer.MAX_VALUE;
+            if (i > 1) {
+                right = dp[i - 2] + Math.abs(height[i] - height[i - 2]);
+            }
+
+            dp[i] = Math.min(left, right);
+
+        }
+        return dp[n - 1];
+
+    }
+
+    static int getCostUsingNoAddSpace(int n, int[] height) {
+        int prev = 0;
+        int prev2 = 0;
+        for (int i = 1; i < n; i++) {
+            int left = prev + Math.abs(height[i] - height[i - 1]);
+            int right = Integer.MAX_VALUE;
+            if (i > 1) {
+                right = prev2 + Math.abs(height[i] - height[i - 2]);
+            }
+
+            int curr = Math.min(left, right);
+
+            prev2 = prev;
+            prev = curr;
+
+        }
+        return prev;
+
     }
 }
